a5e3daa120d8a13636077bd6c3849739c6661482
bergi <bergi@axolotlfarm.org>
2013-09-03 13:52:33 +0200

    adapted tests to new hasAccess function signature

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ describe('single file access control', function() {
 	var ac = new wac({'graphCallback':wac.fileGraphCallback({'baseUrl':'http://example.com/', 'filename':'test/support/access.ttl'})});
 
 	it('should allow GET request to resource http://example.com/allow using agent http://example.com/agent#me', function(done) {
-		ac.hasAccess('GET', 'http://example.com/allow', 'http://example.com/agent#me', null, function(access) {
+		ac.hasAccess('http://example.com/allow', 'GET', 'http://example.com/agent#me', null, function(access) {
 			assert.ok(access);
 
 			done();
@@ -14,7 +14,7 @@ describe('single file access control', function() {
 	});
 
 	it('should deny GET request to resource http://example.com/deny using agent http://example.com/agent#me', function(done) {
-		ac.hasAccess('GET', 'http://example.com/deny', 'http://example.com/agent#me', null, function(access) {
+		ac.hasAccess('http://example.com/deny', 'GET', 'http://example.com/agent#me', null, function(access) {
 			assert.ok(!access);
 
 			done();
@@ -22,7 +22,7 @@ describe('single file access control', function() {
 	});
 
 	it('should deny PUT request to resource http://example.com/allow using agent http://example.com/agent#me', function(done) {
-		ac.hasAccess('PUT', 'http://example.com/allow', 'http://example.com/agent#me', null, function(access) {
+		ac.hasAccess('http://example.com/allow', 'PUT', 'http://example.com/agent#me', null, function(access) {
 			assert.ok(!access);
 
 			done();
@@ -35,7 +35,7 @@ describe('file per directory access control', function() {
 	var ac = new wac({'graphCallback':wac.directoryFileGraphCallback({'basePath':'test/support/', 'baseUrl':'http://example.com/', 'filename':'access.ttl'})});
 
 	it('should allow GET request to resource http://example.com/allow using agent http://example.com/agent#me', function(done) {
-		ac.hasAccess('GET', 'http://example.com/allow', 'http://example.com/agent#me', null, function(access) {
+		ac.hasAccess('http://example.com/allow', 'GET', 'http://example.com/agent#me', null, function(access) {
 			assert.ok(access);
 
 			done();
@@ -43,7 +43,7 @@ describe('file per directory access control', function() {
 	});
 
 	it('should deny GET request to resource http://example.com/deny using agent http://example.com/agent#me', function(done) {
-		ac.hasAccess('GET', 'http://example.com/deny', 'http://example.com/agent#me', null, function(access) {
+		ac.hasAccess('http://example.com/deny', 'GET', 'http://example.com/agent#me', null, function(access) {
 			assert.ok(!access);
 
 			done();
@@ -51,7 +51,7 @@ describe('file per directory access control', function() {
 	});
 
 	it('should deny PUT request to resource http://example.com/allow using agent http://example.com/agent#me', function(done) {
-		ac.hasAccess('PUT', 'http://example.com/allow', 'http://example.com/agent#me', null, function(access) {
+		ac.hasAccess('http://example.com/allow', 'PUT', 'http://example.com/agent#me', null, function(access) {
 			assert.ok(!access);
 
 			done();
